bea72269a60cd50e1219b13fc377987240ecb001
SPettygrove <pettygroves@gmail.com>
2026-01-22 11:30:58 -0500

    Update Wind threshold

--- dwConfig.py
+++ dwConfig.py
@@ -9,7 +9,7 @@ MidLoT = 70     # Above which temp score will be max
 MidHiT = 83     # Above which temp score attenuates to 0 at HiT
 HiT = 95        # Above which overall score will be 0
 LoW = 5         # Below which wind score will be Max. Wind score attenuates to 0 at HiW.
-HiW = 17        # Above which overall score will be 0
+HiW = 13        # Above which overall score will be 0
 LoP = 15         # Below which precip score will be Max. Precip score attenuates to 0 at HiP.
 HiP = 50        # Above which overall score will be 0
 
